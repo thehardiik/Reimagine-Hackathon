008c147669e7167a83304e1923c9799a8ca34691
Hardik Srivastav <hardiknjms21@gmail.com>
2024-06-26 12:28:36 +0530

    Added Cycle Animation

---
+++ script.js
@@ -0,0 +1,14 @@
+var tl = gsap.timeline();
+tl.from(".cycContainer", {
+    x: "-100vw",
+    
+    scrollTrigger: {
+        trigger: '.Main',
+        pin: true,
+        markers: true,
+        scrub: true
+    }
+})
+
+
+
