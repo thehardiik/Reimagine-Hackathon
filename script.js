0fa439b186145e8c9bd881b7b13b9a7b1486bd4b
Hardik Srivastav <hardiknjms21@gmail.com>
2024-06-27 16:50:10 +0530

    First Page and Second Page

--- script.js
+++ script.js
@@ -7,12 +7,62 @@ tl.from("#Life", {
     duration: 1 
 })
 
-tl.from(".cycle", {
+tl.from("#cycle1", {
     y: "100",
     opacity: "0",
-    delay: 0.5,
     duration: 1 
 })
+
+
+gsap.from("#Buy" , {
+    y: "200",
+    opacity: "0",
+    scrollTrigger: {
+        trigger: '.page2',
+        scroller: 'body',
+        start: 'top 40%'
+    }
+})
+
+gsap.from("#cycle2" , {
+    y: "200",
+    opacity: "0",
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.page2',
+        scroller: 'body',
+        start: 'top 40%',
+    }
+})
+
+gsap.to("#cycle2" , {
+    x: "300%",
+    duration: 3,
+    scrollTrigger: {
+        trigger: '.page2container',
+        scroller: 'body',
+        start: "top 15%",
+        scrub: true,
+        //pin: true,
+        //markers: false
+    }
+})
+
+
+gsap.to("#Buy" , {
+    color: '#FF4191',
+    scrollTrigger: {
+        trigger: '.page2container',
+        scroller: 'body',
+        start: "top 15%",
+        end: "top 30$",
+        scrub: true,
+        //pin: true,
+        markers: true
+    }
+})
+
+
 
 
 
