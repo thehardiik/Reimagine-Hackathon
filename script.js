dc0e09d5bd21cdd00ace30102618f3627c14746e
kashisharora9462 <153215875+kashisharora9462@users.noreply.github.com>
2024-06-28 16:30:51 +0530

    c

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 var tl = gsap.timeline();
 
-document.getElementsByClassName("page1").onmousemove = e => {
-    for(const card of document.getElementsByClassName("page1")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+
 
 tl.from("#Life", {
     y: "100",
